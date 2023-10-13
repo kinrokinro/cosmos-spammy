fff8db1a2bd0751f39e702d397e74628b23dae87
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-13 20:48:20 +0800

    organize

--- celatt/2/celattv2.bash
+++ celatt/2/celattv2.bash
@@ -1,72 +1,69 @@
+#!/bin/bash
+
 set -ue
 
-
-# Outer loop grabs sequence number
-while true
-
-do
-# Note: each hex is two bytes, 10,000+10,000=40,000.  Gas is 10 per byte, so 400,000 + default tx gas for the ibc send
+# Constants
 APPNAME="celestia-appd"
-CHANNEL=channel-0
-ADDRESS=celestia1x7jn3tafxdhk844vgle5ga4plyqqxk39z4zsnk
-SEQUENCE=$(curl http://127.0.0.1:5003/cosmos/auth/v1beta1/accounts/$ADDRESS | jq --raw-output ' .account.sequence ')
+CHANNEL="channel-0"
+ADDRESS="celestia1x7jn3tafxdhk844vgle5ga4plyqqxk39z4zsnk"
 IBCMEMO=50000
 RECIEVEADDR=100000
 GAS=3114054
-CHAINID=mocha-4
+CHAINID="mocha-4"
 IBCTIMEOUTS="--packet-timeout-timestamp 0 --packet-timeout-height 0-100000"
 FEES=391405
-UDENOM=utia
-ACCOUNT=72559 # replace with a curl...
+UDENOM="utia"
+# Retrieve account details (Placeholder: Replace with actual curl command if needed)
+ACCOUNT=72559
 
+# Outer loop to handle sequence number
+while true; do
 
-# Inner loop delivers payload until sequence number mismatch then starts again
-while true
+    # Retrieve sequence
+    SEQUENCE=$(curl http://127.0.0.1:5003/cosmos/auth/v1beta1/accounts/$ADDRESS | jq --raw-output ' .account.sequence ')
+    
+    # Inner loop to handle payload delivery
+    while true; do
 
-do
+        echo "Sequence number is $SEQUENCE"
 
-echo "sequence number is $SEQUENCE"
+        # Generate new transaction body with a random string
+        $APPNAME tx ibc-transfer transfer transfer $CHANNEL $ADDRESS 1$UDENOM  \
+            --keyring-backend test --memo $(openssl rand -hex $IBCMEMO) --chain-id $CHAINID --yes $IBCTIMEOUTS \
+            --generate-only --fees $FEES$UDENOM --gas $GAS --from $ADDRESS &> bareibctx.json
+        echo "Transaction body generated with $((IBCMEMO*2)) byte IBC memo field"
 
-# Make a new transaction body with a random string
-$APPNAME tx ibc-transfer transfer transfer $CHANNEL $ADDRESS 1$UDENOM  --keyring-backend test --memo $(openssl rand -hex $IBCMEMO) --chain-id $CHAINID --yes $IBCTIMEOUTS --generate-only --fees $FEES$UDENOM --gas $GAS --from $ADDRESS &> bareibctx.json
-echo "transaction body generated with $((IBCMEMO*2)) byte ibc memo field"
+        # Generate random hex string and set it to the receiver field
+        openssl rand -hex $RECIEVEADDR > tmp.txt
+        echo "Random string generated"
+        jq --rawfile random_str tmp.txt '.body.messages[0].receiver = $random_str' bareibctx.json > autobanana.json
+        echo "$(($RECIEVEADDR*2)) byte random string inserted"
+        
+        # Remove temporary file
+        rm tmp.txt
+        echo "Temporary file removed"
 
-# Step 1: Generate the random hex string and save it to a temporary file
-openssl rand -hex $RECIEVEADDR > tmp.txt
-echo "random string generated"
+        # Sign the transaction
+        $APPNAME tx sign autobanana.json --from $ADDRESS --yes --sequence $SEQUENCE \
+            --chain-id $CHAINID --keyring-backend test --offline --account-number $ACCOUNT &> ban.json
+        echo "Transaction signed"
 
-# Step 2: Use jq with --arg to set the receiver field
-jq --rawfile random_str tmp.txt '.body.messages[0].receiver = $random_str' bareibctx.json > autobanana.json
-echo "$(($RECIEVEADDR*2)) byte random string inserted"
+        # Broadcast the transaction
+        $APPNAME tx broadcast ban.json > banana.log
+        cat banana.log
+        echo "Transaction broadcasted"
 
-# Step 3: remove the temporary file afterwards
-rm tmp.txt
-echo "temporary file removed"
+        # Check for a sequence number mismatch
+        if [ $(grep -c "mismatch" banana.log) -eq 1 ]; then
+            echo "Sequence number mismatch"
+            break
+        fi
 
-# Step 4: Sign the transaction
-$APPNAME tx sign autobanana.json --from $ADDRESS --yes --sequence $SEQUENCE --chain-id $CHAINID --keyring-backend test --offline --account-number $ACCOUNT &> ban.json
-echo "transaction signed"
+        # Update sequence number for next iteration
+        SEQUENCE=$(($SEQUENCE+1))
 
-
-# Step 5: Broadcast the transaction
-$APPNAME tx broadcast ban.json > banana.log
-cat banana.log
-echo "transaction broadcasted"
-
-# Step 6: Check for a sequence number mismatch
-	if [ $(grep -c "mismatch" banana.log) -eq 1 ]
-	then
-		echo "sequence number mismatch"
-		break
-	fi
-
-# iterate sequence number
-SEQUENCE=$(($SEQUENCE+1))
+    done
 
 done
 
-done
-
-echo "if you're running it right the script just restarted"
-
-
+echo "If you're running it right, the script just restarted"
