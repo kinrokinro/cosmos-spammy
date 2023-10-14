ec87f489b7036bffddb72ba2412891fdbe7fe326
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-14 12:59:53 +0200

    update nine

--- nine.bash
+++ nine.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NODE_URL="http://127.0.0.1:6000"
-BATCH_SIZE=30
+BATCH_SIZE=50
 ACCOUNT="72559"
 CHANNEL="channel-1"
 KEY_NAME="test"
@@ -10,12 +10,12 @@ APPNAME="celestia-appd"
 CHAINID="mocha-4"
 IBCMEMO=50000  # 50kb in bytes
 IBCTIMEOUTS="--packet-timeout-timestamp 0 --packet-timeout-height 0-100000"
-FEES="391405"
-GAS="3114054"
+FEES="791405"
+GAS="7114054"
 ADDRESS="celestia1x7jn3tafxdhk844vgle5ga4plyqqxk39z4zsnk"
 
 # Calculate bytes for RECEIVEADDR
-RECIEVEADDR=$(($IBCMEMO * 2))
+RECIEVEADDR=300000
 
 # Get the current block number
 current_block() {
@@ -53,24 +53,31 @@ while true; do
   for ((i=0; i<$BATCH_SIZE; i++)); do
     # Transaction body generation
     $APPNAME tx ibc-transfer transfer transfer $CHANNEL $ADDRESS 1$UDENOM --account-number $ACCOUNT --keyring-backend test --memo $(openssl rand -hex $IBCMEMO) --chain-id $CHAINID --yes $IBCTIMEOUTS --generate-only --fees $FEES$UDENOM --gas $GAS --from $ADDRESS &> bareibctx.json
-    echo "transaction body generated with $((IBCMEMO*2)) byte ibc memo field"
 
     openssl rand -hex $RECIEVEADDR > tmp.txt
     jq --rawfile random_str tmp.txt '.body.messages[0].receiver = $random_str' bareibctx.json > autobanana.json
     rm tmp.txt
-    echo "$(($RECIEVEADDR*2)) byte random string inserted"
 
     # Sign the transaction
     $APPNAME tx sign autobanana.json --account-number $ACCOUNT --from $ADDRESS --yes --sequence $SEQUENCE --chain-id $CHAINID --keyring-backend test --offline &> ban.json
-    echo "transaction signed"
 
     # Broadcast the signed transaction
     $APPNAME tx broadcast ban.json --node $NODE_URL --chain-id $CHAINID &> broadcast.log
+    cat broadcast.log
     echo "transaction broadcasted"
+
+# Check broadcast.log for "code: 20" to indicate mempool fullness
+if cat broadcast.log | grep -q "code: 20"; then
+    echo -e "\e[31mMEMPOOL FULL!!!!!!!!!\e[0m"  # Print in red
+    sleep 60  # Sleep for 60 seconds
+    continue  # Skip the rest of the loop iteration
+fi
+    
 
     # If there's an account sequence mismatch, parse the expected value and use it
 if cat broadcast.log | grep -q "account sequence mismatch"; then
   SEQUENCE=$(cat broadcast.log | grep -oP 'expected \K\d+')
+  echo "we had an account sequence mismatch, adjusting to $SEQUENCE"
 else
   ((SEQUENCE++))
 fi
