9c125f67dd4141c031ae07686e949a80dd749433
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-24 12:39:04 +0200

    changes to key derivation

--- spammy-go/privkey.go
+++ spammy-go/privkey.go
@@ -8,7 +8,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
-	"github.com/cosmos/cosmos-sdk/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func getPrivKey(mnemonic []byte) (cryptotypes.PrivKey, cryptotypes.PubKey, string) {
@@ -29,16 +29,18 @@ func getPrivKey(mnemonic []byte) (cryptotypes.PrivKey, cryptotypes.PubKey, strin
 	pubKey := privKey.PubKey()
 
 	// Convert the public key to Bech32 with custom HRP
-	bech32PubKey, err := bech32ifyPubKeyWithCustomHRP("celestia", pubKey)
+	// bech32PubKey, err := bech32ifyPubKeyWithCustomHRP("celestia", pubKey)
+	// if err != nil {
+	//	panic(err)
+	// }
+
+	addressbytes := sdk.AccAddress(pubKey.Address().Bytes())
+	address, err := sdk.Bech32ifyAddressBytes("cosmos", addressbytes)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Address Ought to be", bech32PubKey)
+	fmt.Println("Address Ought to be", address)
 
-	return privKey, pubKey, bech32PubKey
-}
-
-func bech32ifyPubKeyWithCustomHRP(hrp string, pubKey cryptotypes.PubKey) (string, error) {
-	return types.Bech32ifyAddressBytes(hrp, pubKey.Bytes())
+	return privKey, pubKey, address
 }
